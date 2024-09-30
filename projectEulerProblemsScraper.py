e7f9dc7c6475ac66df9464e5853147d8275a3b2e
Thomas Hasselgreen <thasselgreen@gmail.com>
2024-09-30 18:45:42 +0200

    started work on the scraper

---
+++ projectEulerProblemsScraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from requests.auth import HTTPBasicAuth
+
+#Following a guide fromm https://realpython.com/beautiful-soup-web-scraper-python/
+
+url = "https://projecteuler.net/archives;page=" #The page number will be added to the end of the url using a for loop
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101     Firefox/108.0"} #https://stackoverflow.com/questions/74868953/beautifulsoup-returning-none-when-element-exists
+
+basic = HTTPBasicAuth("scraper", "scraper1") #https://docs.python-requests.org/en/latest/user/authentication/
+
+for i in range (1, 19): #There are 18 pages of problems
+    page = requests.get(url+str(i), headers=headers, auth=basic)
+
+    soup = bs(page.content, "html.parser")
+
+    results = soup.find(id="problems_table")
+    print(results.prettify())
