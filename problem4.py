615dd2548e5684fb2ad05b5a03e1bbb832045373
pickanapple <thasselgreen@gmail.com>
2024-09-30 14:45:07 +0200

    solved problem 4

--- problem4.py
+++ problem4.py
@@ -2,10 +2,18 @@ def isPalindrome(n):
     n = str(n)
     if n[::-1] == n:
         return True
+    
+def palindromeNumber():
+    """
+    Brute forcing the highest palindrome
+    """
+    palindromes = []
 
-for i in range (999, 0, -1):
-    if isPalindrome(i * 999):
-        answer = i * 999
-        break
+    for i in range (999, 800, -1):
+        for j in range (999, 800, -1):
+            if isPalindrome(i * j):
+                palindromes.append(i*j)
 
-print(answer)
+    return max(palindromes)
+
+print(palindromeNumber())
