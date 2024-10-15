662e14c9ecb94ef5ff1cf7e152053c67bf5c880e
pickanapple <thasselgreen@gmail.com>
2024-10-15 12:25:28 +0200

    Worked on list of problems

--- problems/problem10.py
+++ problems/problem10.py
@@ -1,28 +1,5 @@
 primes = [2, 3, 5, 7]
 
-# #if any(t < 0 for t in x
-
-# def isPrime(n):
-#     if any(i < n//2 for i in primes):
-#         if n % i == 0:
-#             return False
-    
-#     primes.append(i)
-#     return True
-    
-# sum = 17
-
-# list = [i for i in range(9, 2_000_000, 2)]
-# print(list)
-
-# for i in list:
-#     print(f"{i}: {sum}")
-#     print(primes)
-#     if isPrime(i):
-#         sum += i
-
-# print(sum)
-
 def isPrime(n):
     if n in primes:
         return True
@@ -40,8 +17,14 @@ def isPrime(n):
         elif i == primes[-1]:
             primes.append(n)
             return True
-        
+    
+sum = 17
 
-for i in range (1000):
-    if  isPrime(i):
-        print(i)
+list = [i for i in range(9, 2_000_000, 2)]
+
+for i in list:
+    #print(f"{i}: {sum}")
+    if isPrime(i):
+        sum += i
+
+print(sum)
