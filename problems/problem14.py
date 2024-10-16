1cdbd9867222892dc0562506583e3e9fbc7c80c9
pickanapple <thasselgreen@gmail.com>
2024-10-16 14:01:40 +0200

    started problem 14

---
+++ problems/problem14.py
@@ -0,0 +1,15 @@
+def collatz(n): 
+    count = 0
+    while n != 1:
+        count += 1
+        if n % 2 == 0:
+            n /= 2
+        else: 
+            n = 3*n + 1
+    
+    return count
+
+maxCount = 0
+
+for i in range(1, 1_000_001):
+    count = 
